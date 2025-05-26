b63259af27077e94183640f8a3009ef98339abc5
ks-tang <pro.tang.kevin@gmail.com>
2025-05-26 23:28:25 +0200

    fix

--- Chatbot_CV_v2/app/main.py
+++ Chatbot_CV_v2/app/main.py
@@ -15,11 +15,13 @@ app = FastAPI()
 base_dir = os.path.dirname(__file__)
 #static_dir = os.path.join(base_dir, "static")
 templates_dir = os.path.join(base_dir, "templates")
-static_path_v2 = os.path.join(os.path.dirname(__file__), "Chatbot_CV_v2", "app", "static")
+static_path_v2 = os.path.join(os.path.dirname(__file__), "static")
+
 
 # Monter les fichiers statiques de la version 2
 #app.mount("/static", StaticFiles(directory=static_dir), name="static")
 app.mount("/static_v2", StaticFiles(directory=static_path_v2), name="static_v2")
+
 
 # Définir le dossier des templates
 templates = Jinja2Templates(directory=templates_dir)
