50e6f7f85c7a8c20830f320b5969dfb7d182ed39
ks-tang <pro.tang.kevin@gmail.com>
2025-05-26 18:53:06 +0200

    fix

--- Chatbot_CV_v2/scripts/rag_engine.py
+++ Chatbot_CV_v2/scripts/rag_engine.py
@@ -1,5 +1,5 @@
-from langchain.vectorstores import FAISS
-from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain.llms import Ollama
 from langchain.chains import RetrievalQA
 
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # main.py (dans /Chatbot_CV)
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
+from fastapi.responses import HTMLResponse
 from Chatbot_CV_v1.app.main import app as app_v1
 from Chatbot_CV_v2.app.main import app as app_v2
 
@@ -18,3 +18,18 @@ app.add_middleware(
 # Monter les deux versions sous des chemins distincts
 app.mount("/v1", app_v1)
 app.mount("/v2", app_v2)
+
+
+
+@app.get("/", response_class=HTMLResponse)
+async def root():
+    return HTMLResponse("""
+        <html>
+        <head><title>Chatbot CV</title></head>
+        <body>
+            <h1>Bienvenue sur le Chatbot CV</h1>
+            <p><a href="/v1">➡️ Accéder à la version 1</a></p>
+            <p><a href="/v2">➡️ Accéder à la version 2</a></p>
+        </body>
+        </html>
+    """)
